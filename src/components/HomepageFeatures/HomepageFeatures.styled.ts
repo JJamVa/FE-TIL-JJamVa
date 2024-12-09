766898c2074489c914e45e16003d328e4ffc78dc
jjamva <hongi97@naver.com>
2024-12-09 18:52:26 +0900

    delete unused components

--- src/components/HomepageFeatures/HomepageFeatures.styled.ts
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-export const FeatureBox = styled.section`
-  background-color: #ffffff;
-`;
--- src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as HomepageHeader } from './HomepageHeader';
-export { default as HomepageFeatures } from './HomepageFeatures';
